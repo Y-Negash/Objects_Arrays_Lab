12853b3b4fcf585bf1a719c10e04b0e800e4d288
Yeabsira Negash <n.yeabsira.a@gmail.com>
2024-03-11 14:26:25 +0000

    completes Q5 of objects.js

--- code/objects.js
+++ code/objects.js
@@ -51,15 +51,19 @@ catBreed = cat.breed;
 dogColour = dog.colours
 dogColourCount = dogColour.length
 
- console.log("The dog has " + dogColourCount + " different colours");
+//  console.log("The dog has " + dogColourCount + " different colours");
 
 /**
  * Q5. I've just bought a 12-year-old African Grey parrot called Lucky. Create an object for her.
  */
 
-parrot = null;
-
-// console.log(parrot);
+parrot = {
+  name: "Lucky",
+  age: 12,
+  breed: "African Grey",
+  colours: "grey"
+};
+ console.log(parrot);
 
 /**
  * Q6. Ella's favourite food is dandelion, add this information to her object
